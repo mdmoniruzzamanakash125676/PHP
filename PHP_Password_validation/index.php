0cc52dddf66849caaa264ba6349c0c8df7fd8781
Md Moniruzzaman <mdmoniruzzamanakash125676@gmail.com>
2024-07-14 14:22:06 +0600

    add

--- PHP_Password_validation/index.php
+++ PHP_Password_validation/index.php
@@ -6,11 +6,27 @@
     <title>From_validation</title>
 </head>
 <body>
-    <form action="welcome.php" method="post">
+    <!-- <form action="welcome.php" method="post">
         Name: <input type="text" name="username">
         Password: <input type="password" name="password">
-        <input type="submit">
-    </form>
+        <input type="submit" >
+    </form> -->
+    <form action="welcome.php" method="post">
+        FName: <input type="text" name="fname">
+        LName: <input type="text" name="lname">
+        Password: <input type="password" name="password">
+        <input type="submit" >
+    </form> 
+
+    <font color="red">
+        <?php 
+        if(isset($_REQUEST["pwd_error"]) ) {
+            echo $_REQUEST["pwd_error"];
+        }
+        elseif(isset($_REQUEST["fname_error"]) ) {
+            echo $_REQUEST["fname_error"];}
+        
+        ?>
 
     
 </body>
--- PHP_Password_validation/welcome.php
+++ PHP_Password_validation/welcome.php
@@ -3,8 +3,12 @@
     $dbUser ="moniruzzaman";
     $dbPassword= "12345";
 
-    $userName=$_REQUEST["username"];
-    $userPassword=$_REQUEST["password"];
+  /*   $userName=$_REQUEST["username"];
+    $userPassword=$_REQUEST["password"]; */
+
+    $fname= htmlspecialchars($_REQUEST["fname"]) ;
+    $lname= $_REQUEST["lname"];
+    $password= $_REQUEST["password"];
 
 
     /* echo "<h1>Result :</h1>";
@@ -15,10 +19,27 @@
         echo "Invalid Password try again";
     } */
 
-    if( $dbPassword== $userPassword){
+   /*  if( $dbPassword== $password){
        // header("location: http://google.com");
         //header("location: profile.php");
         header("location: profile.php?text=$userName");
-    }
+    } */
+   $cheakPwdLen=strlen($password);
+   $cheakfname=strlen($fname);
+
+   echo "<h2>Your Input:</h2>";  
+   echo "FName: " . $fname;  
+   echo "<br>";  
+   echo "LName: " . $lname;  
+   echo "<br>";  
+   echo "Password: " . $password; 
+
+   if (($cheakPwdLen==6)==false){
+    header("location: index.php?pwd_error=password is too short");
+   }
+   elseif (($cheakfname<5)==true){
+    header("location: index.php?fname_error=first name is need is 5 character or above");
+   }
+
 
 ?>
