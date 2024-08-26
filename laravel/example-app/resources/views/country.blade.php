709dc0a97096f943a819f74420599bd284e38ff0
Md Moniruzzaman <mdmoniruzzamanakash125676@gmail.com>
2024-08-26 14:54:10 +0600

    Middleware

---
+++ laravel/example-app/resources/views/country.blade.php
@@ -0,0 +1,1 @@
+<h1>I love My Country</h1>
--- laravel/example-app/routes/web.php
+++ laravel/example-app/routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\DemoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,42 +15,23 @@ use App\Http\Controllers\DemoController;
 */
 
 Route::get('/', function () {
+ 
     return view('welcome');
 });
 
-/*
-Route::get('/hello',function(){
+Route::get('/country', function () {
+ 
+    return view('country');   //middleware
+})->middleware('country');
 
-    return "hellow world";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::get('/name',function(){
-
-    return "my name is monir";
-});
-
-Route::get('/home',function(){
-
-    return view('Homepage');
-});
-
-Route::get('/about',function(){
-
-    return view('AboutPage');
-});
-Route::get('/contact',function(){
-
-    return view('ContactPage');
-});
-*/
-
-///cal by controller;
-Route::get('/home', [HomeController::class,'Home']);
-Route::get('/about', [HomeController::class,'About']);
-Route::get('/contact', [HomeController::class,'Contact']);
-
-
-
-
-Route::get('/name/{firstname}/{secondname}/{thirdname}', [DemoController::class,'MyName']);
+require __DIR__.'/auth.php';
